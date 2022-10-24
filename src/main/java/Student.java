5ead002dd9eaec1d0383c81631bf796882b21f2d
Casanova Geary <CasanovaGeary@Gmail.com>
2022-10-24 14:18:52 -0500

    Initial setup for JUnit.

---
+++ src/main/java/Student.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student
+{
+    long id = 0;
+    String name = "";
+    List<Integer> grades;
+
+    public Student(String name, long id)
+    {
+        this.name = name;
+        this.id = id;
+        grades = new ArrayList<>();
+    }
+
+    // returns the student's id
+    public long getId()
+    {
+
+    }
+
+    // returns the student's name
+    public String getName()
+    {
+
+    }
+
+    // adds the given grade to the grades list
+    public void addGrade(int grade)
+    {
+
+    }
+
+    // returns the list of grades
+    public ArrayList<Integer> getGrades()
+    {
+
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage()
+    {
+
+    }
+}
